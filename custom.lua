f64a6cdd7bdea807152ec6cfedbb6e872e5a906a
axlalexis78 <axlalexis78@gmail.com>
2025-10-17 22:43:04 +0700

    Update animation IDs for player character actions

--- custom.lua
+++ custom.lua
@@ -1,10 +1,10 @@
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=91892322616824"
 Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=91892322616824"
-Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=117175397466382"
-Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=117175397466382"
-Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=91478712085576"
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=73060676441415"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=73060676441415"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=1113752285"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=75020966076281"
-Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=116184760543682"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=122757794615785"
  
 game.Players.LocalPlayer.Character.Humanoid.Jump = false

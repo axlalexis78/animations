6cc30414f8b3ba825083ddf5d952da4eecae0315
axlalexis78 <axlalexis78@gmail.com>
2025-10-17 22:04:18 +0700

    Add files via upload

---
+++ custom.lua
@@ -0,0 +1,10 @@
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=128578785610052"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=128578785610052"
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=117175397466382"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=117175397466382"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=91478712085576"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=75020966076281"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=116184760543682"
+ 
+game.Players.LocalPlayer.Character.Humanoid.Jump = false

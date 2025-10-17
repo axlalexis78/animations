2b7baa49c3068c2f51ad56a9aae59dfd6b3bb3bf
axlalexis78 <axlalexis78@gmail.com>
2025-10-17 19:03:54 +0700

    Add files via upload

---
+++ remove.lua
@@ -0,0 +1,10 @@
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=0"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=0"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
+
+game.Players.LocalPlayer.Character.Humanoid.Jump = false

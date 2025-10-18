1110e6bf124b540dbadbec9821f8f6f2b1ed04c9
axlalexis78 <axlalexis78@gmail.com>
2025-10-18 08:12:17 +0700

    Remove climb animation from character animations

--- remove.lua
+++ remove.lua
@@ -5,6 +5,5 @@ Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=0"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=0"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
-Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
 
 game.Players.LocalPlayer.Character.Humanoid.Jump = false

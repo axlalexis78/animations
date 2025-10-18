5990aa6202282dd6639837b228a378ac038fc9b1
axlalexis78 <axlalexis78@gmail.com>
2025-10-18 18:08:18 +0700

    Update walk and run animation IDs in custom.lua

--- custom.lua
+++ custom.lua
@@ -1,6 +1,6 @@
 local Animate = game.Players.LocalPlayer.Character.Animate
-Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=82260970223217"
-Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=82260970223217"
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=94198749485681"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=94198749485681"
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
 

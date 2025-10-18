ac5ab11953f3ad7527a1f151e8e1199022fbec6c
axlalexis78 <axlalexis78@gmail.com>
2025-10-18 17:59:07 +0700

    Update animation IDs and maintain jump state

--- custom.lua
+++ custom.lua
@@ -1,8 +1,6 @@
 local Animate = game.Players.LocalPlayer.Character.Animate
 Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=82260970223217"
-Animate.walk.WalkAnim.Speed = 1.5
 Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=82260970223217"
-Animate.run.RunAnim.Speed = 1.5
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
 

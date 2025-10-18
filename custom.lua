64799e9eda2d348ed2ba4e0799442f7d7b9c60ab
axlalexis78 <axlalexis78@gmail.com>
2025-10-18 10:35:25 +0700

    Change Jump and Fall animation IDs
    Updated Jump and Fall animation IDs for the character.

--- custom.lua
+++ custom.lua
@@ -3,7 +3,7 @@ Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=95643163365
 Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=95643163365384"
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
-Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=87911577991078"
-Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=87911577991078"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=101023857021941"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=101023857021941"
  
 game.Players.LocalPlayer.Character.Humanoid.Jump = false

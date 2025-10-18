b46b9f1085b6d73e466bc83799e374986bab07ac
axlalexis78 <axlalexis78@gmail.com>
2025-10-18 11:02:07 +0700

    Update remove.lua

--- remove.lua
+++ remove.lua
@@ -3,7 +3,5 @@ Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
 Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
 Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=0"
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=0"
-Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
-Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=0"
 
 game.Players.LocalPlayer.Character.Humanoid.Jump = false

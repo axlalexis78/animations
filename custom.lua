695c54388563326db7d3e8acecd427a67b6d8e23
axlalexis78 <axlalexis78@gmail.com>
2025-10-18 09:58:16 +0700

    Add climbing and swimming animations

--- custom.lua
+++ custom.lua
@@ -5,5 +5,7 @@ Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=812536392
 Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=81253639230607"
 Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=87911577991078"
 Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=87911577991078"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=421121499"
+Animate.swim.SwimAnim.AnimationId = "http://www.roblox.com/asset/?id=421121499"
  
 game.Players.LocalPlayer.Character.Humanoid.Jump = false
